bfc94102cfc46ce0fd782718342d992d10885f0b
kyleroush <roush.kyle@gmail.com>
2018-05-24 11:50:31 -0500

    Create HideAndSeek.js
    Update HideAndSeek.js
    
    Update HideAndSeek.js
    
    Update HideAndSeek.js
    
    Get the Pr number from the Html (#2)
    
    Create manifest.json (#5)
    
    Create the logic to create the check box (#4)
    
    Create the logic to filter and to hide the files (#3)
    
    it works (#6)
    
    hahaha
    
    nits
    
    nits
    
    it works but is very buggy
    
    rearange
    
    fix semi collon and bug and make it work everywhere

---
+++ HideAndSeek.js
@@ -0,0 +1,167 @@
+
+//------------------Utils for all work flows
+
+var localStorageKey = "seeker";
+
+//return the number for the pull request
+function getPullRequestId() {
+  // $(".gh-header-number").innerText
+  return window.location.hostname + window.location.pathname;
+}
+
+/* display previously-saved stored notes on startup
+should return a map in the structure
+{
+"#9(PR#)": {
+  "files": {
+    "README.md" : sha...(commit)
+  }
+}
+*/
+function loadData() {
+  var data = JSON.parse(localStorage.getItem(localStorageKey));
+  if(data == undefined) {
+    data = {};
+    data[getPullRequestId()] = {};
+    data[getPullRequestId()]['files'] = {};
+    localStorage.setItem(data);
+  }
+  return data[getPullRequestId()];
+}
+
+//----------------------Unused utils
+/* Clear all completed files for all pull request from the display/storage */
+function clearAll() {
+  localStorage.removeItem(localStorageKey);
+}
+
+/* Clear all completed files for this pull request from the display/storage */
+function clearPr() {
+  var map = JSON.parse(localStorage.getItem(localStorageKey));
+  map[getPullRequestId()] = undefined;
+  localStorage.setItem(localStorageKey, JSON.stringify(map));
+
+}
+
+
+
+//-----------------------add button
+//add the check boxes to each files files-acations class to make the file is completed or not
+function addCompleteAction() {
+
+  var headers = document.querySelectorAll('.file-header.js-file-header');
+  headers.forEach(function(headers) {
+    var action = headers.querySelector('.file-actions');
+    action.appendChild(createCheckBox(headers.attributes["data-path"].value, "2"));
+  });
+}
+
+//Create a checkBox
+function createCheckBox(filePath, sha) {
+  var span = document.createElement('span');
+  var label = document.createElement('label');
+  var checkBox = document.createElement('input');
+  checkBox.addEventListener( 'click', function() {
+      if(this.checked) {
+        completeFile(checkBox.dataset.filePath, checkBox.dataset.sha);
+        document.body.style.border = "5px solid green";
+
+      } else {
+        unCompleteFile(checkBox.dataset.filePath);
+      }
+  });
+  checkBox.dataset.filePath = filePath;
+  checkBox.dataset.sha = sha;
+  checkBox.querySelector('HideAndSeek');
+  checkBox.type = "checkBox";
+  label.innerText = "Completed";
+  span.appendChild(label);
+  label.appendChild(checkBox);
+  return span;
+}
+
+//This needs to save the data to the local storage
+//should write in this structure
+/*
+{
+"#9(PR#)": {
+  "files": {
+    "README.md" : sha...(commit)
+  }
+}
+*/
+function completeFile(fileName, sha) {
+  var map = JSON.parse(localStorage.getItem(localStorageKey));
+  if (map == undefined) {
+    map = {};
+    map[getPullRequestId()] = {};
+    map[getPullRequestId()]['files'] = {};
+  }
+  map[getPullRequestId()]['files'][fileName] = sha;
+  localStorage.setItem(localStorageKey, JSON.stringify(map));
+}
+
+//remove a file from the local storage
+function unCompleteFile(fileName) {
+  var map = JSON.parse(localStorage.getItem(localStorageKey));
+  if (map == undefined) {
+    map = {};
+    map[getPullRequestId()] = {};
+    map[getPullRequestId()]['files'] = {};
+  }
+  map[getPullRequestId()]['files'][fileName] = undefined;
+  localStorage.setItem(localStorageKey, JSON.stringify(map));
+
+}
+
+
+//----------------------------------Hide Files
+/**
+ * Hide the files.
+ * fileHeaderList: The list of Html elements representing the files headers
+ * Implentation: Click on the collasp button for each Html file header element passed in.
+ */
+function hideCompletedFiles(fileMap) {
+  var fileHeaderList = filterCompletedFiles(fileMap);
+
+  for (var fileHeader in fileHeaderList) {
+    fileHeaderList[fileHeader].querySelector('.file-actions').querySelector('.btn-octicon.p-1.pr-2.js-details-target').click();
+  }
+}
+
+// filter the files that have been stored
+/**
+ * Filter the list of Html file headers elements
+ * fileMap: The map of files names and commits
+ * example
+ * {
+ *    "README.md" : sha...(commit),
+ *    ...
+ * }
+ */
+// TODO: check to see if this file has been updated
+function filterCompletedFiles(fileMap) {
+  var fileHeaderList = document.querySelectorAll('.file-header.js-file-header');
+  var filteredFileHeaderList = [];
+
+  fileHeaderList.forEach(function(fileHeader) {
+    var filePath = fileHeader.attributes["data-path"];
+    if (fileMap[filePath.value] != undefined) {
+      filteredFileHeaderList.push(fileHeader);
+
+    }
+  });
+  return filteredFileHeaderList;
+}
+
+
+//----------------------- Start up
+
+initialize();
+
+// The function called on set up the plugin
+function initialize() {
+
+  addCompleteAction();
+  hideCompletedFiles(loadData()["files"]);
+}
