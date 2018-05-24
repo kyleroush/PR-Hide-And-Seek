c730b7b33f69b05d682fe7942c566ceedbadf551
kyleroush <roush.kyle@gmail.com>
2018-05-24 11:50:31 -0500

    Create HideAndSeek.js

---
+++ HideAndSeek.js
@@ -0,0 +1,75 @@
+
+/* display previously-saved stored notes on startup */
+//should return a map in the structure
+/*
+{
+"#9(PR#)": {
+  "files": {
+    "README.md" : sha...(commit)
+  }
+}
+*/
+function loadAllData() {
+  
+}
+
+//add the check boxes to each files files-acations class to make the file is completed or not
+function addCompleteAction() {
+
+}
+
+//This needs to save the data to the local storage
+//should write in this structure
+/*
+{
+"#9(PR#)": {
+  "files": {
+    "README.md" : sha...(commit)
+  }
+}
+*/
+function completeFile(fileName, sha) {
+
+}
+
+//remove a file from the local storage
+function unCompleteFile(fileName) {
+  
+}
+
+//hide the files
+function hideCompletedFiles(fileList) {
+
+}
+
+// check the files for if there is an update
+function filterCompletedFiles(fileMap) {
+
+}
+
+/* Clear all completed files for all pull request from the display/storage */
+function clearAll() {
+
+}
+
+/* Clear all completed files for this pull request from the display/storage */
+function clearPr() {
+
+}
+
+//return the number for the pull request
+function getPullRequest() {
+  
+}
+
+initialize();
+
+function initialize() {
+// load the load all the data
+// hide the files
+// add the button to all the files 
+
+}
+
+
+
